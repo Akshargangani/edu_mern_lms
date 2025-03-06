0a19d4361cb08622f2f16f5dcc6a0246abf28fd0
your name <your-email@example.com>
2025-03-06 16:53:23 +0530

    Add main application routes and educator navigation handling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useMatch } from 'react-router-dom'
 import Home from './pages/student/Home.jsx'
 import CoursesList from './pages/student/CoursesList.jsx'
 import CourseDetails from './pages/student/CourseDetails.jsx'
@@ -11,11 +11,17 @@ import Dashboard from './pages/educator/Dashboard.jsx'
 import AddCourse from './pages/educator/AddCourse.jsx'
 import MyCourses from './pages/educator/MyCourses.jsx'
 import StudentsEnrolled from './pages/educator/StudentsEnrolled.jsx'
+import Navbar from './components/student/Navbar.jsx'
 
 
 const App = () => {
+
+  const isEducatorRoute = useMatch('/educator/*');
+  
   return (
-    <div>
+    <div className='text-default min-h-screen bg-white'>
+      {!isEducatorRoute &&  <Navbar />}
+     
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/Course-list' element={<CoursesList />}/>
