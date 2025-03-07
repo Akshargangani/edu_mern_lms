3dcecf97722cd39adb36788d16d243dfe5b26c23
your name <your-email@example.com>
2025-03-07 17:49:02 +0530

    Updated CoursesList component: fixed typos and import issues

--- client/src/pages/student/CoursesList.jsx
+++ client/src/pages/student/CoursesList.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
+import { React ,useContext } from 'react'
+import { AppContext } from '../../context/AppContext'
 
 const CoursesList = () => {
+
+  const {navigate} = useContext(AppContext)
   return (
+    <>
     <div>
-        <h1>CoursesList page</h1>
+      <div>
+       <div>
+       <h1>Courde List</h1>
+        <p className='text-gray-500'>
+          <span className='text-blue-600 cursol-pointer'
+          onClick={()=> navigate('/')}>Home</span> / <span>Courde List</span>
+        </p>
+       </div>
+      </div>
     </div>
+    </>
   )
 }
 
