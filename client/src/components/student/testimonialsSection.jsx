71104809edd1f8413742bab7acef86b86399f346
your name <your-email@example.com>
2025-03-07 13:43:52 +0530

    Fixed component name and typo in TestimonialsSection

--- client/src/components/student/testimonialsSection.jsx
+++ client/src/components/student/testimonialsSection.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const testimonialsSection = () => {
   return (
     <div>
-        <h1>testimonialsSection</h1>
+        <h2>testimonialsSection</h2>
+        <p>Hear from out learners as they share their journeys of transformation,
+          success, and how our <br /> platform has made a difference in their lives.
+        </p>
     </div>
   )
 }
